5721325c83d89fc7c5af396652cb743fbccf0371
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-04-09 16:19:43 +0900

    feat: focus-visivle、csx導入、Buttonコンポーネント作成(#3)

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,45 @@
+import { FC, ComponentPropsWithRef } from 'react';
+import { css } from '@emotion/react';
+import { fonts, colors } from '@/styles/constants';
+import { createDarkenColor } from '@/libs/csx';
+
+type Props = ComponentPropsWithRef<'button'>;
+
+const Button: FC<Props> = ({ children, ...props }) => {
+  return (
+    <button css={button} {...props}>
+      {children}
+    </button>
+  );
+};
+
+const button = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 16px;
+  font-family: ${fonts.notoSans};
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px;
+  color: ${colors.white};
+  letter-spacing: -0.035em;
+  cursor: pointer;
+  background-color: ${colors.primary};
+  border: none;
+  border-radius: 8px;
+  transition: background-color 0.3s;
+
+  &:hover,
+  &:focus {
+    /* stylelint-disable-next-line function-name-case, function-no-unknown */
+    background-color: ${createDarkenColor(colors.primary, 0.15)};
+  }
+
+  &:focus:not(.focus-visible) {
+    outline-color: transparent;
+  }
+`;
+
+export default Button;
