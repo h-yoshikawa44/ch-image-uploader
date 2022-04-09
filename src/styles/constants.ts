b66ff9566c7fe098dd04bb4ce0bcba8961a7f03c
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-04-09 15:57:38 +0900

    feat: フォント適用、スタイル定数作成(#3)

---
+++ src/styles/constants.ts
@@ -0,0 +1,50 @@
+// ブレイクポイント
+const xs = 0;
+const sm = 600;
+const md = 960;
+const lg = 1280;
+const xl = 1920;
+
+export const breakPoint = {
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+};
+
+const poppins = 'Poppins, sans-serif';
+const notoSans = 'Noto Sans, sans-serif';
+const montserrat = 'Montserrat, sans-serif';
+
+export const fonts = {
+  poppins,
+  notoSans,
+  montserrat,
+};
+
+const gray2 = '#4F4F4F';
+const gray3 = '#828282';
+const gray4 = '#BDBDBD';
+const gray5 = '#E0E0E0';
+const white = '#fff';
+
+const blue = '#2F80ED';
+const blueLighten = '#97BEF4';
+const green = '#219653';
+
+const primary = blue;
+const success = green;
+
+export const colors = {
+  gray2,
+  gray3,
+  gray4,
+  gray5,
+  white,
+  blue,
+  blueLighten,
+  green,
+  primary,
+  success,
+};
